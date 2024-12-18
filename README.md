55e052b9853d102127c7d3b222b65b8306163d0f
Harper Scott <harpercscott@gmail.com>
2024-12-17 21:29:36 -0500

    Add very basic argument parsing to main()

---
+++ README.md
@@ -0,0 +1,6 @@
+# carp
+## a simple (somewhat filetype agnostic) whitespace stripper
+
+At the moment, this does nothing and is heavily WIP.
+
+Created to work on basic Rust skills and knowledge.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        // TODO: print list of options/flags following line describing the problem
+        println!("Input file required.");
+        return;
+    }
+
+    let filename: String = args[1].to_owned();
+    let filename_split: Vec<&str> = filename.split(".").collect();
+    if filename_split.len() < 2 {
+        println!("Invalid filename.");
+        return;
+    }
+
+    let filetype: String = filename_split[filename_split.len() - 1].to_owned();
+
+    // TODO: replace println!() with function calls for respective filetype (need to be implemented)
+    match filetype.as_str() {
+        "txt" => println!("txt"),
+        "csv" => println!("csv"),
+        _ => println!("File type not currently supported."),
+    }
 }
